86585fa8004fe017522cd18f5b8736884715b0ca
Nikola <nikj1705@gmail.com>
2023-02-05 10:39:52 +0200

    db, entity setup, remove some dependencies

---
+++ src/main/java/com/example/entities/Example.java
@@ -0,0 +1,15 @@
+package com.example.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "example")
+public class Example {
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
